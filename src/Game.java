36c60bf0a7a458cf95815b11277570759ee1b6e1
Thibault <thibault.viaene@mailfence.com>
2022-11-22 15:00:13 +0100

    GameSetUp

--- src/Game.java
+++ src/Game.java
@@ -16,13 +16,23 @@ public class Game {
         this.round = 0;
         this.score = new int[players][2];
         for (int i = 0; i < players; i++) {
-
+            score[i][0] = i;
+            score[i][1] = 0;
         }
     }
 
     public void nextRound (){
         round++;
     }
-
+    public String showRound () {
+        return "Round " + this.round;
+    }
+    public String showScore () {
+        String showScore = "Player \t Score \n";
+        for (int i = 0; i < score.length; i++) {
+            showScore += "Player " + (i+1) + "\t" + score[i][1] + "\n";
+        }
+        return showScore;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * ${PROJECT_NAME} : ${NAME}
  *
@@ -6,6 +8,16 @@
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int players = 0;
+        do {
+            System.out.println("Let's play Evolution! How many players are joining this game? (1-4)");
+            players =  scan.nextInt();
+            scan.nextLine();
+        } while (players < 0 || players > 4);
+        Game newGame = new Game(players);
+        System.out.println("A game has been started with " + newGame.players + " new players, everyone starts with one animal. \n Rules: \n ");
+        System.out.println(newGame.showScore());
 
     }
 }
