5842ed2edbfa7e3b8dc99feb764821dc145355e6
Thibault <thibault.viaene@mailfence.com>
2022-11-22 16:25:09 +0100

    deleted herbi & carnivore

--- src/Carnivore.java
+++
@@ -1,9 +0,0 @@
-/**
- * Evolution : Carnivore
- *
- * @author viaen
- * @version 15/11/2022
- */
-public class Carnivore extends Animal{
-
-}
--- src/Herbivore.java
+++
@@ -1,9 +0,0 @@
-/**
- * Evolution : Herbivore
- *
- * @author viaen
- * @version 15/11/2022
- */
-public class Herbivore extends Animal {
-
-}
