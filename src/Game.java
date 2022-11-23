31e4d8f292bb5ca1357583137ecc8d577f4ed1b8
Thibault <thibault.viaene@mailfence.com>
2022-11-23 20:52:53 +0100

    experiment -player +game

--- src/Game.java
+++ src/Game.java
@@ -8,17 +8,31 @@ public class Game {
     int players;
     int availablePlantFood;
     int round;
-    int[][] score;
-
+    int[] score;
+    int[][] animals;
     public Game(int players) {
         this.players = players;
         this.availablePlantFood = 0;
         this.round = 0;
-        this.score = new int[players][2];
+        this.score = new int[players];
         for (int i = 0; i < players; i++) {
-            score[i][0] = i;
-            score[i][1] = 0;
+            score[i] = 0;
         }
+        this.animals = new int[players][10];
+        for (int i = 0; i < players; i++) {
+            animals[i][0] = 1;
+        }
+    }
+
+    public void newAnimal(int playerX){
+        int[][] animalSwitch = this.animals;
+        this.animals = new int [animalSwitch.length+1][3];
+        for (int i = 0; i < animalSwitch.length; i++){
+            for (int j = 0; j < animalSwitch[i].length; j++){
+                this.animals[i][j] = animalSwitch[i][j];
+            }
+        }
+
     }
 
     public void nextRound (){
@@ -28,7 +42,8 @@ public class Game {
         return "Round " + this.round;
     }
     public String showScore () {
-        String showScore = "Player \t Score \n";
+        String showScore = "Round " + this.round;
+        showScore += "\t Player \t Score \n";
         for (int i = 0; i < score.length; i++) {
             showScore += "Player " + (i+1) + "\t" + score[i][1] + "\n";
         }
