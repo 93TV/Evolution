e93f697aad0cd2fa1e1b663640a9d190addb0182
Thibault <thibault.viaene@mailfence.com>
2022-11-23 23:01:23 +0100

    showAnimals volleditg uit code

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,6 @@ public class Main {
         Game newGame = new Game(players);
         System.out.println("A game has been started with " + newGame.players + " new players, everyone starts with one animal. \n Rules: \n ");
         System.out.println(newGame.showScore());
-
-        for (int i = 0; i <  newGame.players; i++) {
-            newGame.showAnimals(i);
-        }
-
-
+        
     }
 }
